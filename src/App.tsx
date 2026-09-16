9722a741ec4d997a933c0f51f630751f76e84ab6
Artem Strelnikov <piramida1237@gmail.com>
2026-09-16 20:58:25 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Google Search</h1>
+
+      <input
+        type="text"
+        placeholder="Введите ключевую фразу"
+      />
+
+      <button type="button">Найти</button>
+    </>
+  );
+}
+
+export default App;
